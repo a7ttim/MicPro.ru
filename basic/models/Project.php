567ce1f55a904da7d7fa74da5e133cf9a625e05e
Acid_beast17 <attim38@gmail.com>
2017-04-17 15:56:42 +0700

    CRUD added

--- basic/models/Project.php
+++ basic/models/Project.php
@@ -1,4 +1,5 @@
 <?php
+<<<<<<< HEAD
 /**
  * Created by PhpStorm.
  * User: amazi
@@ -21,3 +22,51 @@ class Project extends ActiveRecord
 }
 
 ?>
+=======
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "projects".
+ *
+ * @property integer $project_id
+ * @property string $project_name
+ * @property string $project_date
+ */
+class Project extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'projects';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['project_name', 'project_date'], 'required'],
+            [['project_date'], 'safe'],
+            [['project_name'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'project_id' => 'Project ID',
+            'project_name' => 'Project Name',
+            'project_date' => 'Project Date',
+        ];
+    }
+}
+>>>>>>> CRUD added
